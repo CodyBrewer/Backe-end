463ffac7611ce0c914cc41e3145b2fea95d1653b
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-24 17:03:17 -0700

    fixed merge conflict errors that were crashing app

--- api/strain-rt/strn--rrtr.js
+++ api/strain-rt/strn--rrtr.js
@@ -1,124 +1,81 @@
-const express = require('express')
-const helper = require('./strn-hlpr.js')
-const restricted = require('../auth/authenticate-middleware')
-const strn = express.Router()
+const express = require("express");
+const helper = require("./strn-hlpr.js");
+const restricted = require("../auth/authenticate-middleware");
+const strn = express.Router();
 
-strn.get('/', (req, res) => {
-    helper.find()
+strn.get("/", (req, res) => {
+  helper
+    .find()
     .then(strain => {
-        res.status(200).json(strain)
+      res.status(200).json(strain);
     })
-    .catch(err => res.status(500).json({ error: `internal service issue see: ${err}`}))
-})
-
-strn.get('/:id', restricted, (req, res) => {
-    helper.findById(req.params.id)
-    .then(strain => res.status(200).json(strain))
-    .catch(err => res.status(500).json({ error: `internal service issue see: ${err}`}))
-})
-
-strn.post('/', (req, res) => {
-    console.log(req.body)
-
-const {strain_name, strain_type, strain_desc} = req.body;
-const strain = {strain_name, strain_type, strain_desc};
-const {effects, flavor} = req.body;
-
-
-if(!strain_name || !strain_type) {
-    res.status(400).json({ message: 'please add name and type of new strain' })
-} else if( !effects || !flavor) {
-    res.status(400).json({ message: 'please add effects and flavors new strain' })
-} else {
-    let strain_id;
-    helper.add(strain)
-        .then(strain_idArr => {
-            strain_id = strain_idArr[0]
-            const attribute = {effects, flavor, strain_id}
-            return helper.addAttr(attribute)
-        })
-        .then(() => helper.findById(strain_id))
-        .then(strain => {
-           
-            res.status(201).json(strain)
-        })
-<<<<<<< HEAD
-        .catch(err => res.status(500).json({ error: `internal service issue see: ${err}`}
-        ))
-}
-})
-// strn.put('/:id', async (req, res) => {
-//         const edit = req.body
-//         const { id } = req.params
-//         if (!edit.strain_name || !edit.strain_type) {
-//             return res.status(400).json({message: `no strain with id of ${id}found`})
-//         }
-//         try {
-//             const str = await helper.findById(id)
-           
-//             if (!str) {
-//                 res.status(404).json({message: `no strain with id of ${id}found`})
-//             } else {
-//                 await helper.update(id, edit)
-//                 const uS = await db.findById(id)
-    
-//                 res.status(200).json(uS)
-//             }
-//         } catch (err) {
-//             res.status(500)
-//             .json({ error: 'failed to get users from db'})
-        
-//         }
-// })
-=======
-        .catch(err => res.status(500).json({ error: `internal service issue see: ${err}`}))
-};
->>>>>>> 166578e6031cf70e4120ac1f7467ff3667a71c4e
-
-strn.delete('/:id', async (req, res) => {
-    const id = req.params.id
-    helper.remove(id)
-    .then(rem => {
-        if (rem > 0) {
-            res.status(200).json({message: `Strain with id of ${id} has been removed`, id: id})
-        } else {
-            res.status(404).json({errorMessage: `Strain with ${id} could not be found`})
-        };
-        
-    })
-    .catch(err => {
-        res.status(500)
-        .json({ error: 'failed to delete strain from db'})
-<<<<<<< HEAD
-        })
-})
-
-
-strn.delete('/:id', async (req, res) => {
-    const id = req.params.id
-    helper.remove(id)
-    .then(rem => {
-        if (rem > 0) {
-            res.status(200).json({message: `Strain with id of ${id} has been removed`, id: id})
-        } else {
-            res.status(404).json({errorMessage: `Strain with ${id} could not be found`})
-        }
-        
-    })
-    .catch(err => {
-        res.status(500)
-        .json({ error: 'failed to delete strain from db'})
-        })
-})
-
-module.exports = strn;
-=======
-        });
-    })
+    .catch(err =>
+      res.status(500).json({ error: `internal service issue see: ${err}` })
+    );
 });
 
+strn.get("/:id", restricted, (req, res) => {
+  helper
+    .findById(req.params.id)
+    .then(strain => res.status(200).json(strain))
+    .catch(err =>
+      res.status(500).json({ error: `internal service issue see: ${err}` })
+    );
+});
 
+strn.post("/", (req, res) => {
+  console.log(req.body);
 
+  const { strain_name, strain_type, strain_desc } = req.body;
+  const strain = { strain_name, strain_type, strain_desc };
+  const { effects, flavor } = req.body;
+
+  if (!strain_name || !strain_type) {
+    res.status(400).json({ message: "please add name and type of new strain" });
+  } else if (!effects || !flavor) {
+    res
+      .status(400)
+      .json({ message: "please add effects and flavors new strain" });
+  } else {
+    let strain_id;
+    helper
+      .add(strain)
+      .then(strain_idArr => {
+        strain_id = strain_idArr[0];
+        const attribute = { effects, flavor, strain_id };
+        return helper.addAttr(attribute);
+      })
+      .then(() => helper.findById(strain_id))
+      .then(strain => {
+        res.status(201).json(strain);
+      })
+      .catch(err =>
+        res.status(500).json({ error: `internal service issue see: ${err}` })
+      );
+  }
+
+  strn.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    helper
+      .remove(id)
+      .then(rem => {
+        if (rem > 0) {
+          res
+            .status(200)
+            .json({
+              message: `Strain with id of ${id} has been removed`,
+              id: id
+            });
+        } else {
+          res
+            .status(404)
+            .json({ errorMessage: `Strain with ${id} could not be found` });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: "failed to delete strain from db" });
+      });
+  });
+});
 
 module.exports = strn;
->>>>>>> 166578e6031cf70e4120ac1f7467ff3667a71c4e
--- api/strain-rt/strn-hlpr.js
+++ api/strain-rt/strn-hlpr.js
@@ -1,46 +1,39 @@
-const db = require('../../database/db-config.js');
+const db = require("../../database/db-config.js");
 
 module.exports = {
-    find, 
-    findById,
-    add,
-    addAttr,
-    deleted
-}
+  find,
+  findById,
+  add,
+  addAttr,
+  deleted
+};
 
 function find() {
-    return db
-    .select('*')
-    .from('attributes as a')
-    .join('strains as s', 'a.strain_id', '=', 's.strain_id')
+  return db
+    .select("*")
+    .from("attributes as a")
+    .join("strains as s", "a.strain_id", "=", "s.strain_id");
 }
 
 function findById(s_id) {
-    return db
-        .select('*')
-        .from('attributes as a')
-        .join('strains as s', 'a.strain_id', '=', 's.strain_id')
-        .where('a.strain_id', '=', s_id)
-        .first()
+  return db
+    .select("*")
+    .from("attributes as a")
+    .join("strains as s", "a.strain_id", "=", "s.strain_id")
+    .where("a.strain_id", "=", s_id)
+    .first();
 }
 
 function add(strain) {
-return     db('strains')
-     .insert(strain, 'id')  
+  return db("strains").insert(strain, "id");
 }
 
 function addAttr(attribute) {
-  return  db('attributes')
-    .insert(attribute, 'id')
+  return db("attributes").insert(attribute, "id");
 }
 
 function deleted(id) {
-    return db('strains')
+  return db("strains")
     .where({ id })
-<<<<<<< HEAD
-    .then(result => result)
-}
-=======
     .then(result => result);
 }
->>>>>>> 166578e6031cf70e4120ac1f7467ff3667a71c4e
